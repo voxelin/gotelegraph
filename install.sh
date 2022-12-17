bf8d24d23cd1ba6b74a6c2f8c8cf4aca0a6ef14a
Jenny Poulson <911us@duck.com>
2022-12-17 15:36:47 +0000

    changes in the script :part2

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
-# Check if program is installed
-if [ -f /usr/local/bin/telegraph ]; then
-    echo "telegraph is already installed"
-    exit 0
+#!/bin/bash -e
+if [ -f /usr/bin/telegraph ]; then
+    echo "telegraph is already installed..."
+    echo "reinstalling..."
+    sudo rm /usr/bin/telegraph
 fi
 
 # Installation
-cd /usr/local/bin
-wget https://github.com/voxelin/gotelegraph/releases/download/v0.0.0/telegraph
+cd /usr/bin
+sudo wget https://github.com/voxelin/gotelegraph/releases/download/v0.0.0/telegraph -q --show-progress
+sudo chmod +x telegraph
 
 # Check if installation was successful
-if [ -f /usr/local/bin/telegraph ]; then
+if [ -f /usr/bin/telegraph ]; then
     echo "telegraph was successfully installed"
     exit 0
 else
